01d925dee55cdcb5c2535aad378ae4dca324ceee
mohamad <mohd1haddad@gmail.com>
2017-08-24 20:27:43 +0300

    added password reset locally

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
 
 from .views import (
     account_login,
@@ -9,10 +11,42 @@ from .views import (
 )
 
 urlpatterns = [
+# sign
     url(r'^login/', account_login, name='login'),
     url(r'^logout/', account_logout, name='logout'),
     url(r'^signup/', account_register, name='signup'),
-
+# account    
     url(r'^(?P<username>\w+)/edit/$', account_edit, name='edit'),
     url(r'^(?P<username>\w+)/$', account_detail, name='detail'),
+
+# password_reset
+    url(
+        r'^password/forgot/$', auth_views.password_reset, 
+        {
+            'template_name':'accounts/registration/password_reset_form.html',
+            'email_template_name': 'accounts/registration/password_reset_email.html',
+            'subject_template_name': 'accounts/registration/password_reset_subject.txt',    
+            'post_reset_redirect': 'accounts:password_reset_done',
+        },
+        name='password_reset'
+    ),
+    url(r'^password/forgot/done/$', 
+        auth_views.password_reset_done, 
+        {'template_name':'accounts/registration/password_reset_done.html'},
+        name='password_reset_done'
+    ),
+    url(r'^password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, 
+        {
+            'template_name':'accounts/registration/password_reset_confirm.html',
+            'post_reset_redirect': 'accounts:password_reset_complete',
+
+        },
+        name='password_reset_confirm'
+    ),
+    url(r'^password/reset/done/$', 
+        auth_views.password_reset_complete, 
+        {'template_name':'accounts/registration/password_reset_complete.html'},
+        name='password_reset_complete'
+    ), 
 ]
